4ee8e08406f65bf09b4a3c42ba67569c927870b8
liuxx839 <108911741+liuxx839@users.noreply.github.com>
2024-08-02 10:45:09 +0800

    Update config.py

--- config.py
+++ config.py
@@ -62,7 +62,7 @@ def get_rewrite_system_message(institution, department, person):
 prob_identy_system_message = '''
 You are a Medical Insight quality inspector. Please check if the given materials in json meets the below requriments:
 the info  should cover the 4W elements (Who(Name,Affiliation,department), What, Why, Way Forward), while the private info should be empty for Anonymization purpose.
-just return if any missing in the required fields and non-empty in the private info. no need to re-write. 
+1) just return if any missing in the required fields 2) if private info meets Anonymization. 3)no need to re-write. 
 You need to add appropriate emojis in your response, and the reply should be in Chinese.
 
 '''
