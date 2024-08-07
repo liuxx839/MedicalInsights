6f46a20ceb2215fe488e70da5a7100a1a11c76c7
liuxx839 <108911741+liuxx839@users.noreply.github.com>
2024-08-07 14:17:38 +0800

    Update layout.py

--- layout.py
+++ layout.py
@@ -1,8 +1,7 @@
-#layout.py
 import streamlit as st
 import re
 from utils import match_color, determine_issue_severity, create_json_data
-from config import json_to_dataframe, get_rewrite_system_message, colors,topics,primary_topics_list
+from config import json_to_dataframe, get_rewrite_system_message, colors, topics, primary_topics_list
 
 def setup_layout(
     topics, diseases, institutions, departments, persons,
@@ -14,22 +13,23 @@ def setup_layout(
     st.title("Medical Insights Tagging & Rewrite")
     
     # Sidebar layout
-    user_input, primary_topics, secondary_topics = setup_sidebar(
+    user_input = setup_sidebar(
         topics, primary_topics_list,
-        generate_tag, generate_diseases_tag,
+        generate_tag, generate_diseases_tag, rewrite,
+        prob_identy, generate_structure_data,
         model_choice, client
     )
     
     # Main page layout
     setup_main_page(
-        topics, institutions, departments, persons,
-        rewrite, prob_identy, generate_structure_data,
-        model_choice, client, user_input, primary_topics, secondary_topics
+        institutions, departments, persons,
+        model_choice, client, user_input
     )
 
 def setup_sidebar(
     topics, primary_topics_list,
-    generate_tag, generate_diseases_tag,
+    generate_tag, generate_diseases_tag, rewrite,
+    prob_identy, generate_structure_data,
     model_choice, client
 ):
     with st.sidebar:
@@ -41,73 +41,55 @@ def setup_sidebar(
         </div>
         """, unsafe_allow_html=True)
 
-        user_input = st.text_area("### Enter Medical Insights: ")
+        user_input = st.text_area("### Enter Medical Insights:", key="user_input", height=200)
 
-        if st.button("Generate Tags"):
-            tags = generate_tag(user_input, model_choice, client)
-            unique_tags = list(set(tags.split(",")))
-            st.session_state.tags = ",".join(unique_tags)
+        col1, col2 = st.columns(2)
+        with col1:
+            if st.button("Generate Tags"):
+                tags = generate_tag(user_input, model_choice, client)
+                unique_tags = list(set(tags.split(",")))
+                st.session_state.tags = ",".join(unique_tags)
 
-            disease_tags = generate_diseases_tag(user_input, model_choice, client)
-            unique_disease_tags = list(set(disease_tags.split(",")))
-            st.session_state.disease_tags = ",".join(unique_disease_tags)
+                disease_tags = generate_diseases_tag(user_input, model_choice, client)
+                unique_disease_tags = list(set(disease_tags.split(",")))
+                st.session_state.disease_tags = ",".join(unique_disease_tags)
 
-        primary_topics = st.multiselect("Select Primary Topics", primary_topics_list)
-        
-        secondary_topics = {}
-        if primary_topics:
-            for topic in primary_topics:
-                secondary_topics[topic] = st.multiselect(f"Select Secondary Topics for {topic}", topics[topic])
+        with col2:
+            if st.button("ReWrite"):
+                process_rewrite(user_input, st.session_state.get('institution'), 
+                                st.session_state.get('department'), st.session_state.get('person'), 
+                                model_choice, client, rewrite, generate_structure_data, prob_identy)
 
-    return user_input, primary_topics, secondary_topics
+    return user_input
 
 def setup_main_page(
-    topics, institutions, departments, persons,
-    rewrite, prob_identy, generate_structure_data,
-    model_choice, client, user_input, primary_topics, secondary_topics
+    institutions, departments, persons,
+    model_choice, client, user_input
 ):
-    st.write("### Selected Options")
-    display_topics(primary_topics, secondary_topics)
-
-     # 创建三列
+    # 创建三列
     col1, col2, col3 = st.columns(3)
 
     # 在每列中放置一个选择框
     with col1:
-        institution = st.selectbox("Select Institution", institutions)
+        st.session_state.institution = st.selectbox("Select Institution", institutions)
     
     with col2:
-        department = st.selectbox("Select Department", departments)
+        st.session_state.department = st.selectbox("Select Department", departments)
     
     with col3:
-        person = st.selectbox("Select Title", persons)
+        st.session_state.person = st.selectbox("Select Title", persons)
 
     display_tags()
-
-    if st.button("ReWrite"):
-        process_rewrite(user_input, institution, department, person, model_choice, client,
-                        rewrite, generate_structure_data, prob_identy)
-
     display_rewrite_results()
 
     use_generated_text_and_tags = st.checkbox("Use Editable Rewritten Text and AutoTags", value=True)
 
     st.download_button(
         label="Download JSON",
-        data=create_json_data(use_generated_text_and_tags, st.session_state, user_input, primary_topics),
+        data=create_json_data(use_generated_text_and_tags, st.session_state, user_input, []),
         file_name="medical_insights.json",
         mime="application/json"
     )
-
-def display_topics(primary_topics, secondary_topics):
-    primary_topic_tags = [f'<span class="tag" style="background-color: {colors[topic]};">{topic}</span>' for topic in primary_topics]
-    st.markdown(f"**Primary Topics:** {' '.join(primary_topic_tags)}", unsafe_allow_html=True)
-
-    secondary_topic_tags = []
-    for topic, subtopics in secondary_topics.items():
-        for subtopic in subtopics:
-            secondary_topic_tags.append(f'<span class="tag" style="background-color: {colors[topic]};">{subtopic}</span>')
-    st.markdown(f"**Secondary Topics:** {' '.join(secondary_topic_tags)}", unsafe_allow_html=True)
 
 def display_tags():
     if 'tags' in st.session_state:
@@ -138,10 +120,11 @@ def process_rewrite(user_input, institution, department, person, model_choice, c
 
 def display_rewrite_results():
     if 'rewrite_text' in st.session_state:
-        col1, col2 = st.columns(2)
-        
-        with col1:
-            st.subheader("Assessment Feedback:")
+        st.subheader("Editable Rewritten Text:")
+        user_editable_text = st.text_area("", st.session_state.rewrite_text, height=300)
+        st.session_state.rewrite_text = user_editable_text
+
+        with st.expander("Assessment Feedback"):
             background_color = determine_issue_severity(st.session_state.potential_issues)
             st.markdown(
                 f"""
@@ -151,15 +134,6 @@ def display_rewrite_results():
                 """,
                 unsafe_allow_html=True
             )
-        
-        with col2:
-            st.subheader("Editable Rewritten Text:")
-            user_editable_text = st.text_area("", st.session_state.rewrite_text, height=300)
-            st.session_state.rewrite_text = user_editable_text
-
-        st.subheader("Extracted Information:")
-        if 'table_df' in st.session_state:
-            st.dataframe(st.session_state.table_df)
 
     st.markdown(
         """
