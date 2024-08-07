d3559a70628f6b1ef7f7e59feb9c375ef79440f8
liuxx839 <108911741+liuxx839@users.noreply.github.com>
2024-08-07 09:10:45 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+#main.py
+import streamlit as st
+from layout import setup_layout
+from functions import (
+    setup_client, generate_tag, generate_diseases_tag, rewrite,
+    prob_identy, generate_structure_data
+)
+from config import (
+    topics, diseases, institutions, departments, persons,
+    primary_topics_list, primary_diseases_list
+)
+
+def main():
+    st.set_page_config(layout="wide")
+    
+    model_choice, client = setup_client()
+    
+    setup_layout(
+        topics, diseases, institutions, departments, persons,
+        primary_topics_list, primary_diseases_list,
+        generate_tag, generate_diseases_tag, rewrite,
+        prob_identy, generate_structure_data,
+        model_choice, client
+    )
+
+if __name__ == "__main__":
+    main()
