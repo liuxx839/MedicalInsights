b5a729bf8bd77b190d317e859ba3da56692e7d9f
liuxx839 <108911741+liuxx839@users.noreply.github.com>
2024-08-07 17:02:28 +0800

    Update layout.py

--- layout.py
+++ layout.py
@@ -159,10 +159,6 @@ def display_rewrite_results():
             padding: 5px;
             margin: 2px;
         }
-        .streamlit-expanderHeader {
-            font-size: 18px;
-            font-weight: 600;
-        }
         </style>
         """,
         unsafe_allow_html=True
