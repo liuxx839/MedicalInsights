68d91448754ae7a1cc4e1d222d1ff649b28214fc
liuxx839 <108911741+liuxx839@users.noreply.github.com>
2024-08-13 16:04:00 +0800

    Update layout.py

--- layout.py
+++ layout.py
@@ -79,7 +79,7 @@ def setup_sidebar(
                 st.session_state.disease_tags = ",".join(unique_disease_tags)
 
         with col2:
-            if st.button("Step 3: Rewrite -》"):
+            if st.button("Step 3: Rewrite >>"):
                 process_rewrite(user_input, st.session_state.get('institution'), 
                                 st.session_state.get('department'), st.session_state.get('person'), 
                                 model_choice, client, rewrite, generate_structure_data, prob_identy)
