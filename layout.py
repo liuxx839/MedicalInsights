2c2cb4bdad9bd3212c911313bf502cce30dcff52
liuxx839 <108911741+liuxx839@users.noreply.github.com>
2024-08-15 12:57:00 +0800

    Update layout.py

--- layout.py
+++ layout.py
@@ -153,11 +153,11 @@ def display_rewrite_results():
     # 初始化 st.session_state['rewrite_text']，如果它不存在
     if 'rewrite_text' not in st.session_state:
         st.session_state['rewrite_text'] = ""  # 或者可以初始化为其他默认值   
-    st.subheader("Editable Rewritten Text:")    
-    # 显示文本框，无论 st.session_state['rewrite_text'] 是否存在
-    user_editable_text = st.text_area("", st.session_state['rewrite_text'], height=300)  
-    # 更新 st.session_state['rewrite_text'] 以保存用户输入的内容
-    st.session_state['rewrite_text'] = user_editable_text
+        st.subheader("Editable Rewritten Text:")    
+        # 显示文本框，无论 st.session_state['rewrite_text'] 是否存在
+        user_editable_text = st.text_area("", st.session_state['rewrite_text'], height=300)  
+        # 更新 st.session_state['rewrite_text'] 以保存用户输入的内容
+        st.session_state['rewrite_text'] = user_editable_text
         
         col1, col2 = st.columns([0.75,0.25])
         with col1:
