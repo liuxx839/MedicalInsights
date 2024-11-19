7afd65d21b984458cac6cc66fc9228d95b535afd
liuxx839 <108911741+liuxx839@users.noreply.github.com>
2024-11-19 11:18:15 +0800

    Update layout.py

--- layout.py
+++ layout.py
@@ -61,7 +61,8 @@ def readimg(user_image):
             ],
             model="llama-3.2-11b-vision-preview",
         )
-      return(chat_completion.choices[0].message.content)
+        return chat_completion.choices[0].message.content
+
     except Exception as e:
         raise Exception(f"Error processing image with Groq API: {str(e)}")
 
