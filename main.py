79617c3c908f1d7d16f9695ae0d499be581f8fb3
liuxx839 <108911741+liuxx839@users.noreply.github.com>
2025-04-10 12:49:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from io import BytesIO
 from dagrelation import DAGRelations
 from datadescription import DataDescription
 
-model_choice_research, client_research = setup_client(model_choice = 'glm-4-flash')
+model_choice_research, client_research = setup_client(model_choice = 'glm-4-plus')
  
 def create_mermaid_html_from_edges(dag_edges):
     """
